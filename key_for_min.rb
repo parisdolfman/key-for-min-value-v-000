415dd77f7b07d6b8320e2a495abed950c16fe11b
Paris Dolfman <parisdolfman@gmail.com>
2018-06-19 19:55:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,8 @@
 
 def key_for_min_value(name_hash)
   low_key = nil
-  low_value = Float::INFINITY
   low_value = nil
   name_hash.each do |key, value|
-    if value < low_value
       if low_value == nil || value < low_value
         low_value = value
         low_key = key
@@ -14,4 +12,4 @@ def key_for_min_value(name_hash)
 end
 low_key
 end
-end
+
